b9b6a07d8a229faa119eb1991c9667e3d8b455fd
Kiarash Kia <kiarashkia1999@gmail.com>
2024-07-02 17:46:03 -0400

    A2 Started

---
+++ PersonalityQuiz/PersonalityQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Kiarash Kiya on 2024-07-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
