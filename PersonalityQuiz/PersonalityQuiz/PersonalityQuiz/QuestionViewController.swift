4988df783078142e53aa630472f6d2fa33039956
Kiarash Kia <kiarashkia1999@gmail.com>
2024-07-03 23:06:44 -0400

    Part 5 Update

--- PersonalityQuiz/PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
@@ -11,115 +11,165 @@ class QuestionViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     
     @IBOutlet weak var singleStackView: UIStackView!
-        @IBOutlet var singleButton1: UIButton!
-        @IBOutlet var singleButton2: UIButton!
-        @IBOutlet var singleButton3: UIButton!
-        @IBOutlet var singleButton4: UIButton!
+    @IBOutlet var singleButton1: UIButton!
+    @IBOutlet var singleButton2: UIButton!
+    @IBOutlet var singleButton3: UIButton!
+    @IBOutlet var singleButton4: UIButton!
     
     
     @IBOutlet weak var multipleStackView: UIStackView!
-        @IBOutlet var multiLabel1: UILabel!
-        @IBOutlet var multiLabel2: UILabel!
-        @IBOutlet var multiLabel3: UILabel!
-        @IBOutlet var multiLabel4: UILabel!
+    @IBOutlet var multiLabel1: UILabel!
+    @IBOutlet var multiLabel2: UILabel!
+    @IBOutlet var multiLabel3: UILabel!
+    @IBOutlet var multiLabel4: UILabel!
     
+    @IBOutlet weak var multiSwitch1: UISwitch!
+    @IBOutlet weak var multiSwitch2: UISwitch!
+    @IBOutlet weak var multiSwitch3: UISwitch!
+    @IBOutlet weak var multiSwitch4: UISwitch!
     
     @IBOutlet weak var rangedStackView: UIStackView!
-        @IBOutlet var rangedLabel1Left: UILabel!
-        @IBOutlet var rangedLabel1Right: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
     
-        @IBOutlet var rangedLabel2Left: UILabel!
-        @IBOutlet var rangedLabel2Right: UILabel!
-    
+    @IBOutlet weak var rangedSlider: UISlider!
     
     @IBOutlet var questionProgressView: UIProgressView!
     
     var questions: [Question] = [
-    Question(
-        text: "What type of environment do you prefer?",
-        type: .single,
+            Question(
+                text: "What type of environment do you prefer?",
+                type: .single,
+                answers: [
+                    Answer(text: "Steak", type: .lion),
+                    Answer(text: "Fish", type: .cat),
+                    Answer(text: "Carrots", type: .rabbit),
+                    Answer(text: "Corn", type: .turtle)
+                ]
+            ),
+            Question(
+                text: "Which activities do you enjoy?",
+                type: .multiple,
+                answers: [
+                    Answer(text: "Sleeping", type: .cat),
+                    Answer(text: "Eating", type: .lion),
+                    Answer(text: "Swimming", type: .turtle),
+                    Answer(text: "Cuddling", type: .rabbit)
+                ]
+            ),
+            Question(
+                text: "How much do you enjoy car rides?",
+                type: .ranged,
+                answers: [
+                    Answer(text: "I dislike them", type: .cat),
+                    Answer(text: "I love them", type: .lion),
+                    Answer(text: "I barely notice them", type: .turtle),
+                    Answer(text: "I get a little nervous", type: .rabbit)
+                ]
+            ),
+        ]
         
-        answers: [
-            Answer(text: "Steak", type: .lion),
-            Answer(text: "Fish", type: .cat),
-            Answer(text: "Carrots", type: .rabbit),
-            Answer(text: "Corn", type: .turtle)
-            
-    ]
-    ),
-    
-    
-    Question(
-        text: "Which activities do you enjoy?",
-        type: .multiple,
+        var questionIndex = 0
+        var answersChosen: [Answer] = []
         
-        answers: [
-            Answer(text: "Sleeping", type: .cat),
-            Answer(text: "Eating", type: .lion),
-            Answer(text: "Swimming", type: .turtle),
-            Answer(text: "Cuddling", type: .rabbit)
-            
-    ]
-    ),
-    
-    
-    Question(
-        text: "How much do you enjoy car rides?",
-        type: .ranged,
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            updateUI()
+        }
         
-        answers: [
-            Answer(text: "I dislike them", type: .cat),
-            Answer(text: "I love them", type: .lion),
-            Answer(text: "I barely notice them", type: .turtle),
-            Answer(text: "I get a little nervous", type: .rabbit)
-            
-    ]
-    ),
-    
-    Question(
-        text: "Which is your preference socially?",
-        type: .ranged,
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
         
-        answers: [
-            Answer(text: "Small groups", type: .cat),
-            Answer(text: "New people", type: .lion),
-            Answer(text: "One-to-One communication", type: .turtle),
-            Answer(text: "Energetic gatherings", type: .rabbit)
+        switch sender {
+        case singleButton1:
+            answersChosen.append(currentAnswers[0])
             
-    ]
-    ),
-    ]
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        updateUI()
-        // Do any additional setup after loading the view.
+        case singleButton2:
+            answersChosen.append(currentAnswers[1])
+            
+        case singleButton3:
+            answersChosen.append(currentAnswers[2])
+            
+        case singleButton4:
+            answersChosen.append(currentAnswers[3])
+        default:
+            break
+        }
+        nextQuestion()
     }
     
-    var questionIndex = 0
-    var answerChosen: [Answer] = []
     
+    @IBAction func multipleAnswerButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+        
+        if multiSwitch1.isOn {
+            answersChosen.append(currentAnswers[0])
+        }
+        if multiSwitch2.isOn {
+            answersChosen.append(currentAnswers[1])
+        }
+        if multiSwitch3.isOn {
+            answersChosen.append(currentAnswers[2])
+        }
+        if multiSwitch4.isOn {
+            answersChosen.append(currentAnswers[3])
+        }
+        nextQuestion()
+    }
     
-    func updateUI() {
-        singleStackView.isHidden = true
-        multipleStackView.isHidden = true
-        rangedStackView.isHidden = true
+    @IBAction func rangedAnswerButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+        let index = Int(round(rangedSlider.value * Float(currentAnswers.count - 1)))
         
-        navigationItem.title = "Question #\(questionIndex + 1)"
+        answersChosen.append(currentAnswers[index])
+    
+        nextQuestion()
+    }
+    
+        func updateUI() {
+            singleStackView.isHidden = true
+            multipleStackView.isHidden = true
+            rangedStackView.isHidden = true
+            
+            let currentQuestion = questions[questionIndex]
+            let currentAnswers = currentQuestion.answers
+            let totalProgress = Float(questionIndex) / Float(questions.count)
+            
+            navigationItem.title = "Question #\(questionIndex + 1)"
+            questionLabel.text = currentQuestion.text
+            questionProgressView.setProgress(totalProgress, animated: true)
+            
+            switch currentQuestion.type {
+            case .single:
+                updateSingleStack(using: currentAnswers)
+            case .multiple:
+                updateMultipleStack(using: currentAnswers)
+            case .ranged:
+                updateRangedStack(using: currentAnswers, for: currentQuestion.type)
+            }
+        }
         
-        let currentQuestion = questions[questionIndex]
-        
-        switch currentQuestion.type {
-        case .single:
+        func updateSingleStack(using answers: [Answer]) {
             singleStackView.isHidden = false
             
-        case .multiple:
+            singleButton1.setTitle(answers[0].text, for: .normal)
+            singleButton2.setTitle(answers[1].text, for: .normal)
+            singleButton3.setTitle(answers[2].text, for: .normal)
+            singleButton4.setTitle(answers[3].text, for: .normal)
+        }
+        
+        func updateMultipleStack(using answers: [Answer]) {
             multipleStackView.isHidden = false
             
-        case .ranged:
-            rangedStackView.isHidden = false
+            multiLabel1.text = answers[0].text
+            multiLabel2.text = answers[1].text
+            multiLabel3.text = answers[2].text
+            multiLabel4.text = answers[3].text
         }
-    }
-    
-}
+        
+        func updateRangedStack(using answers: [Answer], for type: ResponseType) {
+            rangedStackView.isHidden = false
+                label1.text = answers.first?.text
+                label2.text = answers.last?.text
+            }
+       }
