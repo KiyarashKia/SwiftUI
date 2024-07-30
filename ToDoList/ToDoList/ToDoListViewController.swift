2ad879ebc810002598c0ee3ccb67253537490892
Kiarash Kia <kiarashkia1999@gmail.com>
2024-07-29 21:44:50 -0400

    Checkpoint 2

--- ToDoList/ToDoList/ToDoListViewController.swift
+++ ToDoList/ToDoList/ToDoListViewController.swift
@@ -11,80 +11,98 @@ import UIKit
 class ToDoListViewController: UITableViewController {
     var toDoItems: [ToDoItem] = []
 
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Register a standard cell
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ToDoItemCell")
-        
-        // Load existing to-do items
-        loadToDoItems()
-        
-        navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return toDoItems.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        let item = toDoItems[indexPath.row]
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .short
-        dateFormatter.timeStyle = .short
-        let dueDateString = dateFormatter.string(from: item.dueDate)
-        cell.textLabel?.text = "\(item.title) - \(dueDateString)"
-        
-        let doneButton = UIButton(type: .system)
-        doneButton.setTitle("Done", for: .normal)
-        doneButton.addTarget(self, action: #selector(markAsDone(_:)), for: .touchUpInside)
-        doneButton.tag = indexPath.row
-        cell.accessoryView = doneButton
-        
-        return cell
-    }
-    
-    @objc func markAsDone(_ sender: UIButton) {
-        let index = sender.tag
-        let indexPath = IndexPath(row: index, section: 0)
-        
-        // Mark the item visually as done (e.g., change text color to gray)
-        let cell = tableView.cellForRow(at: indexPath)
-        cell?.textLabel?.textColor = .gray
-        
-        // Delay removal
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) { [weak self] in
-            guard let self = self else { return }
-            self.toDoItems.remove(at: index)
-            self.saveToDoItems()
-            self.tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-    }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the item from the array
-            toDoItems.remove(at: indexPath.row)
+            super.viewDidLoad()
+            // Register a standard cell
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ToDoItemCell")
             
-            // Save the updated array to UserDefaults
-            saveToDoItems()
-            
-            // Delete the row from the table view
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            // Add an Edit button to the navigation bar
+            navigationItem.rightBarButtonItem = self.editButtonItem
+        activityIndicator.isHidden = true
         }
-    }
-    
-    func saveToDoItems() {
-        if let encodedData = try? JSONEncoder().encode(toDoItems) {
-            UserDefaults.standard.set(encodedData, forKey: "toDoItems")
-        }
-    }
 
-    func loadToDoItems() {
-        if let savedData = UserDefaults.standard.data(forKey: "toDoItems"),
-           let decodedItems = try? JSONDecoder().decode([ToDoItem].self, from: savedData) {
-            toDoItems = decodedItems
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return toDoItems.count
+        }
+
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+            let item = toDoItems[indexPath.row]
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .short
+            dateFormatter.timeStyle = .short
+            let dueDateString = dateFormatter.string(from: item.dueDate)
+            cell.textLabel?.text = "\(item.title) - \(dueDateString)"
+            return cell
+        }
+
+        // Enable swipe to delete on the trailing side
+        override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+            // Create the "Delete" action
+            let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completionHandler) in
+                self.toDoItems.remove(at: indexPath.row)
+                self.saveToDoItems()
+                tableView.deleteRows(at: [indexPath], with: .fade)
+                completionHandler(true)
+            }
+            
+            
+            // Return the swipe actions
+            return UISwipeActionsConfiguration(actions: [deleteAction])
+        }
+
+        // Add a swipe action for marking items as done on the leading side
+        override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+            // Create the "Done" action
+            let doneAction = UIContextualAction(style: .normal, title: "Done") { (action, view, completionHandler) in
+                self.markItemAsDone(at: indexPath)
+                completionHandler(true)
+            }
+            doneAction.backgroundColor = .blue
+            
+            // Return the swipe actions
+            return UISwipeActionsConfiguration(actions: [doneAction])
+        }
+
+        // Function to mark an item as done
+        func markItemAsDone(at indexPath: IndexPath) {
+            _ = toDoItems[indexPath.row]
+            
+            // Temporarily change the appearance of the cell to indicate it's done
+            if let cell = tableView.cellForRow(at: indexPath) {
+                cell.contentView.backgroundColor = .lightGray
+                cell.textLabel?.text = "Done and Disappearing.."
+                cell.textLabel?.textColor = .white
+                activityIndicator.isHidden = false
+            }
+
+            // Show the activity indicator
+            activityIndicator.startAnimating()
+            
+            // Delay removal of the item
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                self.toDoItems.remove(at: indexPath.row)
+                self.saveToDoItems()
+                self.tableView.deleteRows(at: [indexPath], with: .fade)
+                
+                // Hide the activity indicator
+                self.activityIndicator.stopAnimating()
+                
+            }
+        }
+
+        func saveToDoItems() {
+            if let encodedData = try? JSONEncoder().encode(toDoItems) {
+                UserDefaults.standard.set(encodedData, forKey: "toDoItems")
+            }
+        }
+
+        func loadToDoItems() {
+            if let savedData = UserDefaults.standard.data(forKey: "toDoItems"),
+               let decodedItems = try? JSONDecoder().decode([ToDoItem].self, from: savedData) {
+                toDoItems = decodedItems
+            }
         }
     }
-}
